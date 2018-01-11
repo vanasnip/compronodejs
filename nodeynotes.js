c369ffd7772fbadf550c57e942a0b534740f359c
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-11 12:30:05 +0000

    Node Core, server technology

--- nodeynotes.js
+++ nodeynotes.js
@@ -1,24 +1,24 @@
-/*
-No is written in c++
-the javascript engine is written in c++
-*/
+//::::Node Core::::
+//server technology
 
-/*
-JS Asside, 
-Javascript aside: Javascript engines and the ecmascript specificatoin
-there are many engines out there, v8 is only one of them.
-Ecmascript is a standard to which all the different engines comply
-*/
+//Server : a computer that performs services 
+//Client : requests a services and the server does the work
 
-/*
-Javascript Engine: A program that converts javascript code
- into something the computer processor can understand
- Following the ecmascript language.
-*/
+// SO the client is asking for the work to be done and the server does the work
 
-/*
-V8 under the hood:
-Open source, chrome initial
-v8 can be embedded into any c++ program because v8 is a c++ program
-*/
+//**this relationship is what is known as : 
+//***CLIENT SERVER MODEL OF COMPUTING */
 
+//EG Web server, that sits on the internet and accepting requests for work and doing that work
+// aslo accepting requests for data and doing any work necessary to deliver/serve that data!
+
+//the standard format for this request and response, question and answer on the web and that HTTP
+//__________________________________HTTP  Request >>>>>___________________________________________
+//__________________________________<<<<< HTTP Response __________________________________________
+
+//The idea of giving javascrip more powers isnt new but is what node does
+
+// typically you would write php, ruby and or c# when coding, switching between the 
+// client side and server side || front end and back end credentials to get the work done
+
+//The goal with nodejs is to write JS on the web server... the paradgm being you only need one programming language
