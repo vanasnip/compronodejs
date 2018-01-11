a0727e7e268ed151543de9ce61801979856a8462
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-11 11:59:22 +0000

    No is written in c++

---
+++ nodeynotes.js
@@ -0,0 +1,24 @@
+/*
+No is written in c++
+the javascript engine is written in c++
+*/
+
+/*
+JS Asside, 
+Javascript aside: Javascript engines and the ecmascript specificatoin
+there are many engines out there, v8 is only one of them.
+Ecmascript is a standard to which all the different engines comply
+*/
+
+/*
+Javascript Engine: A program that converts javascript code
+ into something the computer processor can understand
+ Following the ecmascript language.
+*/
+
+/*
+V8 under the hood:
+Open source, chrome initial
+v8 can be embedded into any c++ program because v8 is a c++ program
+*/
+
