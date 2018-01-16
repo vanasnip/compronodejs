1ffd748e567987d76da06722b92bee0d6584068e
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-16 21:28:17 +0000

    Requiring Native Core Modules

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-// Export vs Module.exports
-// highlighting a quirk in javascript rooted in the nature of passing objects by reference
-// essentially the same object
+// Requiring Native Core Modules
+var util = require('util'); // no dot slash, if its a native node method it just runs
 
-//exports doesnt work for all the patterns we saw before.
-
-var greet = require('./greet');//bad news
-var greet2 = require('./greet2');// alternative / solution
-
-// long story short just use modules.exports
+var name = 'Ivano';
+var greeting = util.format('Hello, %s', name);
+util.log(greeting);
--- greet2.js
+++
@@ -1,9 +0,0 @@
-// it is possible on the other hand to mutate exports, adding properties and methods
-exports.greet = function(){
-    console.log('Hello');
-}
-// then we are sitting pretty..
-
-console.log(exports);
-console.log(module.exports);
-console.log('---------------------------');
