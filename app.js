20b3dd29a5f62c673535ba880198e7195a329003
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-17 11:58:10 +0000

    inheriting from the prototype chain : NOTES

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 //inheriting from the prototype chain
+
+//getting modules from node
 var EventEmiter = require('events');
 var util = require('util');
 
+//defining a function constuctor
 function Greetr(){
     this.greeting = 'Hello World';
 }
 
+//using util from node core to add the method we want
 util.inherits(Greetr, EventEmiter);
 
+// adding my own method onto the changed Greetr
 Greetr.prototype.greet = function(data){
     console.log(this.greeting + ': ' + data);
     this.emit('greet', data);
 }
 
+// instantiate a Greetr object
 var greeter1 = new Greetr();
 
+// using events from node core
 greeter1.on('greet', function(data){
     console.log('Someone greeted', data);
 })
