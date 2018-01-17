2689010479380892844af54bc322c7a4f8951c2e
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-17 10:10:46 +0000

    Let's build a the node event emitter part1

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-//Events and the events emitter
-//Events: something that has happened in our app that we can respond to
+//Let's build a the node event emitter part1
+var Emitter = require('./emitter');
 
-//In node we talk about two different kind of events
+var emtr = new Emitter();
 
-//:::1- system events : c++ core libuv
-//eg. 'I've finished reading a file', 'the file is open', 'I've received date from the internet'. These are things that javascript doesnt have, c++ wrapping on the javascript engine means we can now access those events
+emtr.on('greet', function(){ 
+    // behaving like listener but just function sitting on array;
+    console.log('Somewhere, someone said hello.');
+});
 
-//:::2- custom events : Javascript core | Event emmitter(custom events)
-//A javascript library of events 
-//C++ core libuv sends lower level events to the javascript core(event emitter)
+emtr.on('greet', function(){
+    console.log('A greeting occured!');
+});
 
-//often times when an event happens in libuv it generates a custom event for the javascript engine so we can make use of it. thats what it's there for however it is important to make the distinction between the two..
-
-// JS side is faking it, there is not real eventing or event object. create our own event library.
+console.log('hello');
+emtr.emit('greet');
