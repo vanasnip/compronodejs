4fdca1cf67bbf6d077cd78580ee20d18f3398f4c
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-17 14:56:29 +0000

    Inheriting from event emitter part 3

--- app.js
+++ app.js
@@ -1,31 +1,9 @@
-//inheriting from the prototype chain
+'use strict';
+var Greetr = require('./greetr');
 
-//getting modules from node
-var EventEmiter = require('events');
-var util = require('util');
-
-//defining a function constuctor
-function Greetr(){
-    //::::::::::::::::::::::::::::::::::::::::::::::::::::
-    EventEmiter.call(this); //Behaves like a superclass incase it wants to add props and methods to our obj
-    this.greeting = 'Hello World';
-}
-
-//using util from node core to add the method we want
-util.inherits(Greetr, EventEmiter);
-
-// adding my own method onto the changed Greetr
-Greetr.prototype.greet = function(data){
-    console.log(this.greeting + ': ' + data);
-    this.emit('greet', data);
-}
-
-// instantiate a Greetr object
 var greeter1 = new Greetr();
 
-// using events from node core
 greeter1.on('greet', function(data){
-    console.log('Someone greeted', data);
+    console.log(`Someone greeted ${data}`);
 })
-
-greeter1.greet('Ivano');
+greeter1.greet('Ivano!');
