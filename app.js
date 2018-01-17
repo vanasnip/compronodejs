8b47387c98598a5e223ae3caf603a7e49f1c4141
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-17 10:54:42 +0000

    Solving the MAGIC STRINGS issue

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
-//Let's build a the node event emitter part2
-//Because property names are the same as the node core
-//we just need to delete our emitter and we are going to use nodes'
+//NOTE: MAGIC STRING -- a string that has some special meaning in
+//our code. relying on strings to dynamically access properties for instance
+// is problematic because this is subject to typos and a probably
+// won't be flagged as an error and if it does the IDE cannot tell us that
+// is doesnt recognise a property;
 
-var Emitter = require('events');//<< node events file.
+
+
+var Emitter = require('events');
+
+//solving the MAGIC STRINGS issue...
+var eventConfig = require('./config').events;
+//Everywhere where i had greet I replace with eventsConfig.GREET
 
 var emtr = new Emitter();
 
 //the on method too a string and a listener
-emtr.on('greet', function(){ 
+emtr.on(eventConfig.GREET, function(){ 
     // behaving like listener but just function sitting on array;
     console.log('Somewhere, someone said hello.');
 });
 
-emtr.on('greet', function(){
+emtr.on(eventConfig.GREET, function(){
     console.log('A greeting occured!');
 });
 
 console.log('hello');
 //the emit method just took a string
-emtr.emit('greet');
+emtr.emit(eventConfig.GREET);
+
 
 
