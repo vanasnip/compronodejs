8b2a03c308ef88bd71cc40094e1a7392d65fddc2
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-17 13:46:57 +0000

    Inherit all attached props and methods

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-//call and apply
-// let me change what the this keyword points to 
-var obj = {
-    name: 'John Doe',
-    greet: function(){
-        console.log(`Hello ${ this.name }`);
-    }
+//inheriting from the prototype chain
+
+//getting modules from node
+var EventEmiter = require('events');
+var util = require('util');
+
+//defining a function constuctor
+function Greetr(){
+    //::::::::::::::::::::::::::::::::::::::::::::::::::::
+    EventEmiter.call(this); //Behaves like a superclass incase it wants to add props and methods to our obj
+    this.greeting = 'Hello World';
 }
 
-obj.greet();
-//call overwrites what the this keyword point to?
-//.call({obj:'obj'},param1,param2,param3)
-obj.greet.call({name: 'Jane Doe'});
+//using util from node core to add the method we want
+util.inherits(Greetr, EventEmiter);
 
-//apply does the same as call except in implementation of params
-//.apply({obj:'obj'},[param1,param2,param3])
-obj.greet.apply({name: 'Jane Doe'});
+// adding my own method onto the changed Greetr
+Greetr.prototype.greet = function(data){
+    console.log(this.greeting + ': ' + data);
+    this.emit('greet', data);
+}
+
+// instantiate a Greetr object
+var greeter1 = new Greetr();
+
+// using events from node core
+greeter1.on('greet', function(data){
+    console.log('Someone greeted', data);
+})
+
+greeter1.greet('Ivano');
