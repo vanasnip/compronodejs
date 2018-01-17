17b453ce96d091e203858008a897d804a7880b1e
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-17 15:59:36 +0000

    ::Asynchronous code, libuv, the event loop, streams and more

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-'use strict';
-var Greetr = require('./greetr');
+//::Asynchronous code, libuv, the event loop, streams and more
+//Javascript is synchronous! js just rocks it one thing at a time
+//Node is asynchronous for sure, the v8 JS engine does not!
 
-var greeter1 = new Greetr();
+//when we discuss asynchronous JS we are really talking about what 
+//the Javascript engine is sitting inside of ie the browser or node
 
-greeter1.on('greet', function(data){
-    console.log(`Someone greeted ${data}`);
-})
-greeter1.greet('Ivano!');
+//libuv, the event loop, and non-blocking asynchronous code
+
+//Buffer:
+//A temporary holding spot for data being moved from on place to another.
+
+//Stream:
+//A sequence of data made available over time. 
+//Pieces of data that eventually combine into a whole
