00112afca0b33ef92068ddc25d2978a138bfa1e3
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-18 15:15:24 +0000

    Stream - Readable & Writeable

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
-//Files and fs
-//buffers and callbacks
-var fs = require('fs');//node core
+//Streams
+//Chunks: data is split in chunks and streamed
+//Streams are event emiters have access to on and emit
+//Old stream code and new stream code.
+//now there is a variety of streams
+//Stream .Readbale, .Writable, .Duplex, .Transfom, .PassThrough
+//they are what they sound like
+//Streams are an abstract class, a type of constructer you never work directly work with, but inherit from.
 
-var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
-// __dirname is a param in the c++ wrapper that I have access to
-//.readFileSync is node telling us that this is a synchronous operation
-//can be useful for required config file for instance.
-console.log(greet);
+var fs = require('fs');
+//the stream will fill a buffer with contents
+//if the contents are the same size or smaller than the buffer
+//then we'll just get all the data
+//if its bigger than the buffer size then we will get peices or the data so read data, fill buffer, emit peice repeat, until we have all the data
+var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding:'utf8', highWaterMark: 16 * 1024 });
 
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
 
-//in many cases we dont want this the be a synchronous operation
-//to make it async we use .readFile, it takes the same params as ..Sync + a callback
-var greet2 = fs.readFile(__dirname + '/greet.txt', 'utf8',
-function(err, data){ // error first callbacks node concept
-    console.log('Async: ',data);
-});
-
-//proof that its Async 
-console.log('Done!');
-
-//always lean to the async option!
-
-//We still have a problem in the we are using a buffer and
-//it just sits in memory in something called the heap in V8, node sticks it there. 
-
-//this is why we need streams..
-
-
-
-
-
-
+readable.on('data', function(chunk){
+    console.log(chunk.length);
+    writable.write('#####################################################################################################################################################################################################################################################################################################################################\n' 
+    + chunk + '\n');
+    //just copied the files by listening for the data event because a stream is an event emitter
+})
