3eaf04e3dd9bf541983c60245562f8f6815a2dc2
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-22 10:26:38 +0000

    APIs, endpoints and outputting JSON

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-//Streams and performance
+//APIs and Endpoints
+//APIs on the web tend to accept and receive data via urls.
+//That url is what we refer to as an endpoint
+
+//Outputting JSON
+
 var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res){ 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.htm').pipe(res);
-   // streams are about reading data a chunck at a time
-    // we can send to the any writable stream
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var obj = {
+        firstname: 'John',
+        lastname: 'Doe'
+    };
+
+    res.end(JSON.stringify(obj));
+//Serialize: translating an object into a format that can be store or transfered
     
 }).listen(1337, '127.0.0.1');
