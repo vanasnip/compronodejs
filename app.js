1e776f758d40ee8bb715b6756f8294a08e887bf4
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-22 15:20:19 +0000

    Templates and template engines

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-//Static files and middleware
-//Middlware mean code or software that sits between two layers of software
-// express provides a way to induce middleware into the mix
+//Templates and template engines
+
 
 var express = require('express');
-var path = require('path');
 var app = express();
 var public = '/public';
 
@@ -12,18 +10,19 @@ var public = '/public';
 var port = process.env.PORT || 3000;
 app.use('/assets', express.static('public'))
 
+app.set('view engine', 'ejs');
+
 app.use('/',function(req, res, next){
     console.log('Request URL: ' + req.url);
     next();
 })
 
-
-//HTTP Method / VERBS : GET, POST, DELETE
 app.get('/', function(req, res){
-    res.send('<html><head><link href=../assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World!</h1></body></html>');
-});
+    res.render('index');
+})
+
 app.get('/person/:id', function(req, res){
-    res.send('<html><head><link href=../assets/style.css type=text/css rel=stylesheet /></head><body><h1>Person : ' + req.params.id + '</h1></body></html>');
+    res.render('person',{ID:req.params.id})
 });
 app.get('/api', function(req, res){
     res.json({
