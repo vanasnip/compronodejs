8917d46b080f13b3739ea7e12bac84ae208893d2
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-22 11:17:21 +0000

    ::NPM:The node package manager and semantic versioning

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-//Routing
-//Mapping HTTP requests to content
+//::NPM:The node package manager and semantic versioning
+//packeges and package managers
+//Package is just a collection of code,
+//Package management system is software that automates installing and updating packages: it also managed dependecies 
 
-var http = require('http');
-var fs = require('fs');
+//Semantic versioning (semver) : just looking at the version give us some kind of information
 
-http.createServer(function(req, res){
-    console.log(res.url);
-    if(req.url === '/'){
-        fs.createReadStream(__dirname + '/index.htm').pipe(res);
-    } 
-    else if (req.url === '/api'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        var obj = {
-            firstname: 'John',
-            lastname: 'Doe'
-        };
-
-        res.end(JSON.stringify(obj));
-    } else {
-        res.writeHead(404);
-        fs.createReadStream(__dirname + '/nourl.htm').pipe(res);
-    }
-    
-}).listen(1337, '127.0.0.1');
+// basic rule of samantic versioning are as follows 
+// major.minor.patch // 1.7.2
+//patch: 1.7.*3* bug fixes, other peoples code depending will still work
+//minor: 1.*8*.0 added features and peoples code depending will still work 
+//major: *2*.0.0 `big changes means peoples code depending will probably break!
