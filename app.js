54c004f7fb2d047cce6645da0c57cc6c4ea749c0
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-22 12:32:52 +0000

    NPM and NPM registry: Other peoples code

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-//::NPM:The node package manager and semantic versioning
-//packeges and package managers
-//Package is just a collection of code,
-//Package management system is software that automates installing and updating packages: it also managed dependecies 
+//NPM and NPM registry: Other peoples code
+// npm comes with node.
+//when you download a package from npm you download it from the npm registry
 
-//Semantic versioning (semver) : just looking at the version give us some kind of information
+//INIT, nodemon and package.json
 
-// basic rule of samantic versioning are as follows 
-// major.minor.patch // 1.7.2
-//patch: 1.7.*3* bug fixes, other peoples code depending will still work
-//minor: 1.*8*.0 added features and peoples code depending will still work 
-//major: *2*.0.0 `big changes means peoples code depending will probably break!
+var moment = require('moment'); //the require function looks in node_modules and finds the moment folder.
+console.log(moment().format());
