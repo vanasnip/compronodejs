62968a7e3d83a1ddc5e78c4cd6bc47234db1cd20
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-22 13:13:45 +0000

    Express : Routing

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//::Installing express and making it easier to build a web server
+//Express : Routing
 var express = require('express');
 var app = express();
 //Environment vaiables: global vars specific to the enviro(server) our code is living
@@ -7,7 +7,10 @@ var port = process.env.PORT || 3000;
 
 //HTTP Method / VERBS : GET, POST, DELETE
 app.get('/', function(req, res){
-    res.send('<html><head></head><body><h1>Hello World!</h1></body></html>')
+    res.send('<html><head></head><body><h1>Hello World!</h1></body></html>');
+});
+app.get('/person/:id', function(req, res){
+    res.send('<html><head></head><body><h1>Person : ' + req.params.id + '</h1></body></html>');
 });
 app.get('/api', function(req, res){
     res.json({
