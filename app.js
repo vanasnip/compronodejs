0ba3cfc15343763c50e8ef799175e87ccf11707f
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-22 11:02:49 +0000

    Routing | Mapping HTTP requests to content

--- app.js
+++ app.js
@@ -1,20 +1,25 @@
-//APIs and Endpoints
-//APIs on the web tend to accept and receive data via urls.
-//That url is what we refer to as an endpoint
-
-//Outputting JSON
+//Routing
+//Mapping HTTP requests to content
 
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res){ 
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    var obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-    };
+http.createServer(function(req, res){
+    console.log(res.url);
+    if(req.url === '/'){
+        fs.createReadStream(__dirname + '/index.htm').pipe(res);
+    } 
+    else if (req.url === '/api'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var obj = {
+            firstname: 'John',
+            lastname: 'Doe'
+        };
 
-    res.end(JSON.stringify(obj));
-//Serialize: translating an object into a format that can be store or transfered
+        res.end(JSON.stringify(obj));
+    } else {
+        res.writeHead(404);
+        fs.createReadStream(__dirname + '/nourl.htm').pipe(res);
+    }
     
 }).listen(1337, '127.0.0.1');
