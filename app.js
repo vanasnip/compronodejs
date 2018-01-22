02bf816ae1bd4636ad8973248e0c7bde2e46e218
Ivan Mamenemuno <ivan.m@ims-evolve.com>
2018-01-22 14:43:40 +0000

    Static files and middleware

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
-//Express : Routing
+//Static files and middleware
+//Middlware mean code or software that sits between two layers of software
+// express provides a way to induce middleware into the mix
+
 var express = require('express');
+var path = require('path');
 var app = express();
+var public = '/public';
+
 //Environment vaiables: global vars specific to the enviro(server) our code is living
 //a way for the server to set the enviro var
 var port = process.env.PORT || 3000;
+app.use('/assets', express.static('public'))
+
+app.use('/',function(req, res, next){
+    console.log('Request URL: ' + req.url);
+    next();
+})
+
 
 //HTTP Method / VERBS : GET, POST, DELETE
 app.get('/', function(req, res){
-    res.send('<html><head></head><body><h1>Hello World!</h1></body></html>');
+    res.send('<html><head><link href=../assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World!</h1></body></html>');
 });
 app.get('/person/:id', function(req, res){
-    res.send('<html><head></head><body><h1>Person : ' + req.params.id + '</h1></body></html>');
+    res.send('<html><head><link href=../assets/style.css type=text/css rel=stylesheet /></head><body><h1>Person : ' + req.params.id + '</h1></body></html>');
 });
 app.get('/api', function(req, res){
     res.json({
